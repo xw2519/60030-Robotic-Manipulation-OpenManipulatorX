3f1f8e8eebe95c67587c6b38d58eec8e479a49a2
Xin Wang <59951374+xw2519@users.noreply.github.com>
2022-02-26 14:25:17 +0000

    animated FK simulation added

---
+++ Task 1 - Simulation/matlab scripts/task_1_d.m
@@ -0,0 +1,12 @@
+%% --------------------------------------------------------------------- %%
+% task_1_d.m
+%
+% Task 1 - Modelling the Robot 
+%
+% Re-run your simulation with the inverse kinematics.  
+ 
+% Get your robot to trace a square of 10 x 10 cm in each cartesian plane. 
+% Be sure to draw the square (use line objects of different colours). 
+%% --------------------------------------------------------------------- %%
+
+
