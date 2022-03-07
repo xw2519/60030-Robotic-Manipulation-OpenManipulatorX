5c670283b1605fd443c1ff5489eb98f7cc5ac594
Xin Wang <59951374+xw2519@users.noreply.github.com>
2022-03-07 01:56:03 +0000

    Cleaned up repo

--- Task 1 - Simulation/matlab scripts/testing area/testing.m
+++
@@ -1,4 +0,0 @@
-for i = 1:4
-    a(i) = subplot(2,2,i);
-    b(i) = annotation('textbox','String',"\theta_4",'Position',a(i).Position,'Vert','top','FitBoxToText','on', 'HorizontalAlignment', 'right')
-end
--- src/utils/read_servo_data.m
+++ src/utils/read_servo_data.m
@@ -26,10 +26,82 @@ if ~libisloaded(lib_name)
 end
 
 %% --- Load setup script and save variables --- %
-% Saves the environment variables into env.mat file
-setup_robotic_arm
+%% ---- Load Control Table Addresses ---- %%
+% Control table address is different in every Dynamixel model
 
-%% --- Cancellation button --- %%
+ADDR_PRO_TORQUE_ENABLE       = 64;          % Torque Enable(64) determines Torque ON/OFF. Writing ‘1’ to Toque Enable’s address will turn on the Torque           
+ADDR_PRO_GOAL_POSITION       = 116;         % Sets desired position
+ADDR_PRO_PRESENT_POSITION    = 132;         % Present position of the servo
+ADDR_PRO_OPERATING_MODE      = 11;          % Sets between Current, Velocity, Position Control Mode 
+ADDR_PRO_VELOCITY            = 112;         % Controls the velocity of the servos
+
+%% ---- Load Dynamixel Devices ---- %%
+DXL_ID1_BaseRotation                = 11;          
+DXL_ID2_Shoulder                    = 12;    
+DXL_ID3_Elbow                       = 13;          
+DXL_ID4_Wrist                       = 14;    
+DXL_ID5_Gripper                     = 15;          
+
+%% ---- Load Other Settings ---- %%
+% Protocol version
+PROTOCOL_VERSION            = 2.0;          % See which protocol version is used in the Dynamixel
+
+% Default setting
+BAUDRATE                    = 115200;
+DEVICENAME                  = 'COM3';       % Check which port is being used on your controller
+                                            % ex) Windows: 'COM1'   Linux: '/dev/ttyUSB0' Mac: '/dev/tty.usbserial-*'      
+TORQUE_ENABLE               = 1;            % Value for enabling the torque
+TORQUE_DISABLE              = 0;            % Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = -150000;      % Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 150000;       % and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20;           % Dynamixel moving status threshold
+DXL_VELOCITY                = 20000;
+
+COMM_SUCCESS                = 0;            % Communication Success result value
+COMM_TX_FAIL                = -1001;        % Communication Tx Failed
+
+%% --- Initialize PortHandler Structs --- %%
+% Set the port path
+% Get methods and members of PortHandlerLinux or PortHandlerWindows
+port_num = portHandler(DEVICENAME);
+
+% Initialize PacketHandler Structs
+packetHandler();
+
+index = 1;
+dxl_comm_result = COMM_TX_FAIL;             % Communication result
+
+dxl_error = 0;                              % Dynamixel error
+dxl_present_position = 0;                   % Present position
+
+% Open port
+if (openPort(port_num))
+    fprintf('Port Open\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to open the port\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+% Set port baudrate
+if (setBaudRate(port_num, BAUDRATE))
+    fprintf('Baudrate Set\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to change the baudrate!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+%% --- Limit the speed of the robot --- %%
+write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID1_BaseRotation, ADDR_PRO_VELOCITY, DXL_VELOCITY);
+write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID2_Shoulder, ADDR_PRO_VELOCITY, DXL_VELOCITY);
+write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID3_Elbow, ADDR_PRO_VELOCITY, DXL_VELOCITY);
+write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID4_Wrist, ADDR_PRO_VELOCITY, DXL_VELOCITY);
+write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID5_Gripper, ADDR_PRO_VELOCITY, DXL_VELOCITY); 
+
+disp("Log: Servo speeds limited")
 
 %% ---- Switch Servo Torque Off to Allow Manual Tracking---- %%
 % Put actuator into Position Control Mode
